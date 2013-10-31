17dabdf2fc1753b8f12617f6197ccdd0e0d34674
Alejandro Ardila Schickler <atejandr@gmail.com>
2013-10-31 13:40:10 -0500

    Add Checkin feature View Logic

--- src/com/moodstream/activity/EventDetailsActivity.java
+++ src/com/moodstream/activity/EventDetailsActivity.java
@@ -3,21 +3,24 @@ package com.moodstream.activity;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
+import android.content.IntentSender;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,12 +33,20 @@ import android.widget.TextView;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.json.jackson2.JacksonFactory;
 //import com.google.api.client.util.IOUtils;
 import com.moodstream.R;
+import com.moodstream.activity.CreateEventActivity.ErrorDialogFragment;
 import com.moodstream.adapter.PhotoListAdapter;
+import com.moodstream.model.checkinendpoint.Checkinendpoint;
+import com.moodstream.model.checkinendpoint.model.Checkin;
 import com.moodstream.model.eventendpoint.model.Event;
 import com.moodstream.model.photoendpoint.Photoendpoint;
 import com.moodstream.model.photoendpoint.model.CollectionResponsePhoto;
@@ -45,7 +56,9 @@ import com.moodstream.util.AWSUtils;
 import com.moodstream.util.DateUtils;
 import com.moodstream.util.LocationUtils;
 
-public class EventDetailsActivity extends Activity {
+public class EventDetailsActivity extends Activity  implements LocationListener,
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener{
 	
 	private static final String TAG="EventDetailsActivity";
 	
@@ -60,12 +73,14 @@ public class EventDetailsActivity extends Activity {
 	private static final int ACTION_TAKE_PHOTO=1;//Take a big photo
 	private boolean isCheckedIn=false;
 	
+	private Checkin checkin;
 	
 	private TextView detail_event_name;
 	private TextView detail_event_invitees;
 	private TextView detail_event_date;
 	private TextView detail_event_address;
 	private Button take_photo_btn;
+	
 	
 	//ListView
     private ListView photoList;
@@ -76,7 +91,28 @@ public class EventDetailsActivity extends Activity {
 	private File mPhotoFile;
 	private String timeStamp;
 	
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
 	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mLocationClient.connect();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mLocationClient.disconnect();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mLocationClient.disconnect();
+	}
+	
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -102,7 +138,12 @@ public class EventDetailsActivity extends Activity {
 		detail_event_date.setText("Date: "+DateUtils.getDateFormatString(DateUtils.DATE_FORMAT,selectedEvent.getDate()));
 		
 		take_photo_btn=(Button)findViewById(R.id.take_photo_btn);
-		setButtonListeners();
+		
+		mLocationClient=new LocationClient(this, this, this);
+		
+		new isCheckedinTask().execute();
+		
+		//setButtonListeners();
 		
 		photoList=(ListView)findViewById(R.id.photosList);
 		
@@ -172,14 +213,49 @@ public class EventDetailsActivity extends Activity {
 	
 	private void setButtonListeners()
 	{
-		take_photo_btn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				//Go to a cam activity
-				takePictureIntent(ACTION_TAKE_PHOTO);
+		
+		//mLocationClient.connect();
+		getCurrentLocation();
+		LatLng eventPosition = new LatLng(selectedEvent.getLocation()
+				.getLatitude(), selectedEvent.getLocation().getLongitude());
+		
+		LatLng currentPostition=new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+		
+		if(LocationUtils.distanceBetween(currentPostition, eventPosition, LocationUtils.DISTANCE_IN_METERS)<=150)
+		{
+			if(isCheckedIn)
+			{	
+				Log.d(TAG,"Setting take photo text");
+				take_photo_btn.setText("Take photo");	
+				take_photo_btn.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						//Go to a cam activity
+						takePictureIntent(ACTION_TAKE_PHOTO);
+					}
+				});
 			}
-		});
+			else
+			{
+				Log.d(TAG,"Setting checkin text");
+				take_photo_btn.setText("Checkin");	
+				
+				take_photo_btn.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						
+						new CheckinTask().execute();
+						
+					}
+				});
+			}
+		}
+		else
+		{
+			take_photo_btn.setVisibility(View.GONE);
+		}
 	}
 	
 	
@@ -320,7 +396,188 @@ public class EventDetailsActivity extends Activity {
 			photoList.setAdapter(adapter);
 			
 		}
+	}
 	
+	
+	private class CheckinTask extends AsyncTask<Void, Void, Void>
+	{
+		ProgressDialog dialog;
+		
+		@Override
+		protected void onPreExecute() {
+			dialog=new ProgressDialog(EventDetailsActivity.this);
+			dialog.setCancelable(false);
+			dialog.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			Checkin checkin=new Checkin();
+			checkin.setUserId(currentUsr.getNickname());
+			checkin.setEventId(selectedEvent.getKey().getId());
+			Calendar cal=Calendar.getInstance();
+			checkin.setTimestamp(cal.getTimeInMillis());
+			
+			
+			Checkinendpoint.Builder builder=new Checkinendpoint.Builder(AndroidHttp.newCompatibleTransport(), new JacksonFactory(), null);
+			builder=CloudEndpointUtils.updateBuilder(builder);
+			Checkinendpoint endpoint=builder.build();
+			
+			try {
+				Log.d(TAG,"Checking in...");
+				endpoint.insertCheckin(checkin).execute();
+				Log.d(TAG,"Checked in...");
+			} catch (Exception e) {
+				Log.d(TAG,"Exception occured during insert checkin...");
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			isCheckedIn=true;
+			setButtonListeners();
+			dialog.dismiss();
+		}
 		
 	}
+	
+	private class isCheckedinTask extends AsyncTask<Void, Void, Void>
+	{
+
+		ProgressDialog dialog;
+		
+		@Override
+		protected void onPreExecute() {
+			dialog=new ProgressDialog(EventDetailsActivity.this);
+			dialog.setCancelable(false);
+			dialog.show();
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+		
+			Checkinendpoint.Builder builder=new Checkinendpoint.Builder(AndroidHttp.newCompatibleTransport(), new JacksonFactory(), null);
+			builder=CloudEndpointUtils.updateBuilder(builder);
+			Checkinendpoint endpoint=builder.build();
+			
+			try {
+			    checkin=endpoint.isCheckedin(currentUsr.getNickname(), selectedEvent.getKey().getId()).execute();
+			    
+			    if(checkin.getUserId()!=null)
+			    	isCheckedIn=true;
+			    else
+			    	isCheckedIn=false;
+			    
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			setButtonListeners();
+			dialog.dismiss();
+		}
+		
+		
+	}
+
+	//_______________________________________________________________________________
+	//LOCATION_IMPLEMENTATION________________________________________________________
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		
+		if (connectionResult.hasResolution()) {
+			try {
+
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+
+			} catch (IntentSender.SendIntentException e) {
+
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+
+			// If no resolution is available, display a dialog to the user with
+			// the error.
+			Log.d(TAG,""+connectionResult.getErrorCode());
+		}
+		
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		getCurrentLocation();
+		//mLocationClient.disconnect();
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void getCurrentLocation() {
+		// If Google Play Services is available
+		if (servicesConnected())
+			mCurrentLocation = mLocationClient.getLastLocation();	
+	}
+	
+	/**
+	 * Verify that Google Play services is available before making a request.
+	 * 
+	 * @return true if Google Play services is available, otherwise false
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@SuppressLint("NewApi")
+	private boolean servicesConnected() {
+
+		// Check that Google Play services is available
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+
+			// In debug mode, log the status
+			Log.d(TAG, "Play services available");
+
+			// Continue
+			return true;
+
+			// Google Play services was not available for some reason
+		} else {
+
+			// Display an error dialog
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode,
+					this, 0);
+			if (dialog != null) {
+				dialog.show();
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(dialog);
+				errorFragment.show(getFragmentManager(), TAG);
+			}
+			return false;
+		}
+	}
+
 }
