871bc87d17c0fd12cbabda10f631cbae492d372f
atejandro <atejandro@gmail.com>
2014-03-05 22:37:07 -0500

    Update AWSUtils.java

--- src/com/moodstream/util/AWSUtils.java
+++ src/com/moodstream/util/AWSUtils.java
@@ -4,8 +4,6 @@ public class AWSUtils {
 public static final String AUDIENCE="server:client_id:295173411808-0rihfll869kme3o6kpl3llc2dk13gp25.apps.googleusercontent.com";
 	
 	//AWS S3 CREDENTIALS & CONSTANTS
-	public static String ACCESS_KEY_ID = "AKIAI2HQRUQNPA6TWV7A";
-	public static final String SECRET_KEY = "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";
 	public static final String PICTURE_BUCKET = "moodstream2-bucket";
 		
 	public static String getPictureBucket()
